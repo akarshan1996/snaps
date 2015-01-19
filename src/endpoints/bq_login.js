f328e6a76a019cda83cc75528d096975201f6da1
Carlo Francisco <carlo@groupon.com>
2015-01-19 16:55:27 -0600

    Fix traceur issues (and also add some clarity to login errors).
    Fixes #4.

--- src/endpoints/bq_login.js
+++ src/endpoints/bq_login.js
@@ -7,20 +7,25 @@ export function bqLogin(username, password, timestamp, reqToken, request, baseUr
   }
 
   return new Promise((resolve, reject) => {
+    var error;
     request({
       "uri": baseUrl + '/bq/login',
       "qs": loginParams,
       "method": "POST",
       "timeout": 2000
     }, (err, httpResponse, body) => {
-      var parsedBody = JSON.parse(body);
+      try {
+        var parsedBody = JSON.parse(body);
+      } catch (err) {
+        error = err;
+      }
       if (err || httpResponse.statusCode != 200 || parsedBody.status === -100) {
         if (err) {
-          var error = err;
+          error = err;
         } else if (httpResponse.statusCode === 200) {
-          var error = new Error("Incorrect username and password combo.");
+          error = new Error("Incorrect username and password combo.");
         } else {
-          var error = new Error("Status code of login request was " + httpResponse.statusCode);
+          error = new Error("Status code of login request was " + httpResponse.statusCode);
         }
         reject(error);
       } else {
